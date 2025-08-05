6b96b93b55778e89c8028cdee5d88a4f56b22451
CSESCEM <office.cs@sahyadri.edu.in>
2025-08-05 14:39:28 +0530

    todays comit

---
+++ REACT/fourth_react_app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Counter from './components/Counter'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <h1>Redux</h1>  
+              <Counter/>
+        </div>
+        )
+       
+}
+
+export default App
